7082e45cf323c20f6ebe6a84b95ae922f8ce9730
M!lkyWeb <65228360+MilkyWebaxy97@users.noreply.github.com>
2021-08-27 04:07:11 -0700

    JS practice
    jhjkbkjhjvvk

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+let js = 'amaz';
+console.log("Milkan");
+console.log(23);
+
+let firstName="Milkan";
+console.log(firstName);
+
+let Mili_Haki="Milkan";
+let $function=27;
+console.log($function);
+console.log(Mili_Haki);
+
+let country = "Bangladesh";
+let continent = "Asia";
+let population = "160";
+console.log(country);
+console.log(continent);
+console.log(population); 
+*/
+
+let javascriptIsFun=true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 'Milkan');
+console.log(typeof 33);
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
